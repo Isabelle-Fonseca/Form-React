00899a48198d2d927c8d92eeb183aa1df50dc6b7
Isabelle Fonseca <isabelle7536@gmail.com>
2024-10-29 22:55:59 -0300

    Formulario Finalizado

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -12,6 +12,8 @@ const Form = () => {
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
 
+    setErros(null);
+
     const data: User = {
         name,
         email,
@@ -21,9 +23,13 @@ const Form = () => {
     const validateErrors = validate(data);
 
     if(Object.keys(validateErrors).length > 0){
-        alert("Tem erros!");
+        setErros(validateErrors);
         return;
     }
+
+    setName("");
+    setEmail("");
+    setAgree(false);
 
     alert("Muito obrigado por se inscrever!")
   };
@@ -32,18 +38,21 @@ const Form = () => {
   <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
     <div className="flex flex-col">
         <label className="text-sm text-text-label mb-1" htmlFor="name">Nome:</label>
-        <input type="text" placeholder="Digite o seu nome" className="bg-bg-input rounded-md py-3 px-3 text-sm placeholder:text-sm placeholder:text-text-input focus:ring focus:ring-purple" value={name} onChange={(e) => setName(e.target.value)}/>
+        <input type="text" placeholder="Digite o seu nome" className="bg-bg-input rounded-md py-3 px-3 text-sm text-white placeholder:text-sm placeholder:text-text-input focus:ring focus:ring-purple" value={name} onChange={(e) => setName(e.target.value)}/>
+        {erros?.name && <small className="text-xs text-red-500 mt-1">{erros?.name}</small>}
     </div>
     <div className="flex flex-col">
         <label className="text-sm text-text-label mb-1" htmlFor="name">E-mail:</label>
-        <input type="email" placeholder="Digite o seu e-mail" className="bg-bg-input rounded-md py-3 px-3 text-sm placeholder:text-sm placeholder:text-text-input focus:ring focus:ring-purple" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input type="email" placeholder="Digite o seu e-mail" className="bg-bg-input rounded-md py-3 px-3 text-sm text-white placeholder:text-sm placeholder:text-text-input focus:ring focus:ring-purple" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        {erros?.email && <small className="text-xs text-red-500 mt-1">{erros?.email}</small>}
     </div>
     <div className="flex flex-col">
         <a href="#" className="text-xs underline mb-2 text-purple">Leia os termos</a>
-        <div className="flex gap-2 items-center mb-2">
+        <div className="flex gap-2 items-center mb-1">
             <input type="checkbox" checked={agree} onChange={(e) => setAgree(e.target.checked)}/>
             <label className="text-sm text-text-label" htmlFor="agree">Concordo com os termos</label>
         </div>
+        {erros?.agree && <small className="text-xs text-red-500 mb-1">{erros?.agree}</small>}
         <button type="submit" className="bg-purple hover:bg-purple-light font-medium text-sm py-2 px-4 rounded-md text-white">Cadastrar</button>
     </div>
   </form>
