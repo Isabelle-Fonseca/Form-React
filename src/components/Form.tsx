583ed226dd2d294824c08fe2abac7434a8b5168b
Isabelle Fonseca <isabelle7536@gmail.com>
2024-10-29 21:12:19 -0300

    Foi realizada a configuração das cores e a refatoração dos estilos. Melhorado o layout e o visual.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,25 @@
 
 const Form = () => {
-  return <form>
-    <div>
-        <label>Nome:</label>
-        <input type="text" placeholder="Digite o seu nome" />
+  return( 
+  <form className="flex flex-col gap-3">
+    <div className="flex flex-col">
+        <label className="text-sm text-text-label mb-1" htmlFor="name">Nome:</label>
+        <input type="text" placeholder="Digite o seu nome" className="bg-bg-input rounded-md py-3 px-3 text-sm placeholder:text-sm placeholder:text-text-input focus:ring focus:ring-purple"/>
     </div>
-    <div>
-        <label>E-mail:</label>
-        <input type="email" placeholder="Digite o seu e-mail" />
+    <div className="flex flex-col">
+        <label className="text-sm text-text-label mb-1" htmlFor="name">E-mail:</label>
+        <input type="email" placeholder="Digite o seu e-mail" className="bg-bg-input rounded-md py-3 px-3 text-sm placeholder:text-sm placeholder:text-text-input focus:ring focus:ring-purple" />
     </div>
-    <div>
-        <a href="#">Leia os termos</a>
-        <div>
+    <div className="flex flex-col">
+        <a href="#" className="text-xs underline mb-2 text-purple">Leia os termos</a>
+        <div className="flex gap-2 items-center mb-2">
             <input type="checkbox" />
-            <label>Concordo com os termos</label>
+            <label className="text-sm text-text-label" htmlFor="agree">Concordo com os termos</label>
         </div>
-        <button>Cadastrar</button>
+        <button type="submit" className="bg-purple hover:bg-purple-light font-medium text-sm py-2 px-4 rounded-md text-white">Cadastrar</button>
     </div>
   </form>
+  );
 }
 
 export default Form
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-base': '#5d586e', 
+        'bg-form': '#2b2738', 
+        'bg-input': '#3b364c', 
+        'text-input': '#5d596b', 
+        'purple': '#6e54b5', 
+        'text-label': '#8c8994', 
+        'purple-light': '#7c74b7' 
+      },
+    },
   },
   plugins: [],
 }
